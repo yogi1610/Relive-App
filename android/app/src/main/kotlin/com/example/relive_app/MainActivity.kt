047f7fd8bb172ac0cbc495deb6a5de40a9871769
yogeshbutani <yogeshbutani16@gmail.com>
2025-09-06 09:10:20 +0530

    05/09/25 Project Setup

---
+++ android/app/src/main/kotlin/com/example/relive_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.relive.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+  static const appColor = Color(0xFF00C3B9);
+  static const colorBlack = Color(0xFF000000); // for main texts
+  static const colorWhite = Color(0xFFFFFFFF); // for apps background
+  static const hintColor = Color(0xFFC9C9C9); // for hint texts, icon
+  // static const hintColor = Color(0xFFF9FAFB);// for containers background
+  // static const hintColor = Color(0xFFFAFDFD); // for containers background
+  // static const hintColor = Color(0xFF555555); // for subtitle king of texts
+}
